011ed5193e8013189e315cfb862eeb3b4e7babfe
rrishabhchoudhary <111534887+rrishabhchoudhary@users.noreply.github.com>
2024-04-02 18:15:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+var express = require('express');
+var router = express.Router();
+const userModel = require('./users');
+const postModel = require('./post');
+const passport = require('passport');
+
+
+const localStrategy = require("passport-local").Strategy;
+passport.use(new localStrategy(userModel.authenticate()));
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/profile', isLoggedIn, function(req, res, next){
+  res.send("profile")
+});
+
+router.post("/register", function(req, res) {
+  const { username, email, fullname } = req.body;
+  const userData = new userModel({ username, email, fullname });
+
+  userModel.register(userData, req.body.password).then(function(){
+
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/profile");
+    })
+  })
+})
+
+router.post("/login", passport.authenticate("local",{
+  successRedirect: "/profile",
+  failureRedirect: "/"
+}),function(req, res){ 
+});
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/profile', isLoggedIn, function(req, res, next){
+  res.send("profile")
+});
+
+router.post("/register", function(req, res) {
+  const { username, email, fullname } = req.body;
+  const userData = new userModel({ username, email, fullname });
+
+  userModel.register(userData, req.body.password).then(function(){
+
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/profile");
+    })
+  })
+})
+
+router.post("/login", passport.authenticate("local",{
+  successRedirect: "/profile",
+  failureRedirect: "/"
+}),function(req, res){ 
+});
+
+router.get("/logout", function(req, res, next){
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
+module.exports = router;
+//module.exports = isLoggedIn;
+
+
+
+
+
